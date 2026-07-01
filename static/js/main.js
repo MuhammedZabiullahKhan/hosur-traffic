7cacf95320bf72acc2074e32ba02b3f59afc523e
primesyssolutions <100826803+MuhammedZabiullahKhan@users.noreply.github.com>
2026-07-01 14:26:41 +0530

    Update API_BASE to include '/api' endpoint

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@
 // Created by Shri Muhammed Zabiullah Khan @Axion_Z_Squad
 // ============================================================
 
-const API_BASE = 'https://hosur-traffic.onrender.com';
+const API_BASE = 'https://hosur-traffic.onrender.com/api';
 let currentData = null;
 let updateInterval = null;
 let audioCtx = null;
